273fce3e8c3b324b7877124ea09eaf5de5fe2120
Rakshitha Gowda <rakshithaaahere@gmail.com>
2026-07-10 14:44:11 +0530

    Implement dynamic homepage with featured recipes and categories

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Recipe, Category
 
 
 def home(request):
-    return render(request, "recipes/home.html")
+    featured_recipes = Recipe.objects.filter(featured=True)[:6]
+    categories = Category.objects.all()
+
+    context = {
+        "featured_recipes": featured_recipes,
+        "categories": categories,
+    }
+
+    return render(request, "recipes/home.html", context)
